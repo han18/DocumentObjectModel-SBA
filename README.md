86741fa3e57b738b94ec1766bd98c20482af3d8c
han18 <han.fadah@gmail.com>
2024-10-04 09:05:50 -0400

    creating new style

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DocumentObjectModel-SBA
+
+# This SBA is a form application that takes input from a user
+# the form clear th input 
+# most of the functionality is in the js file 
--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 // SBA 316: The Document Object Model Project
 
 "use strict";
-// to test if the file is connected and console logging 
-console.log("testing"); 
-
+// to test if the file is connected and console logging
+console.log("testing");
 
 let menuLinks = [
-    { text: "home", href: "/home" },
-    { text: "about", href: "/about" },
-    { text: "catalog", href: "/catalog" },
-    { text: "gallery", href: "/ gallery" },
-    { text: "account", href: "/account" },
-  ];
+  { text: "home", href: "/home" },
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "gallery", href: "/ gallery" },
+  { text: "account", href: "/account" },
+];
 
-  // calling the menu bar.. id in the html
+// calling the menu bar.. id in the html
 const navMenu = document.getElementById("menu");
 navMenu.style.height = "100%";
 navMenu.style.backgroundColor = "#0f496b4d";
@@ -21,16 +20,15 @@ navMenu.classList.add("flex-around");
 
 // creating an anchor element for the navbar
 for (let x = 0; x < menuLinks.length; x++) {
-    // creating new element a tag into the nav menu
-    const aLink = document.createElement("a");
-    aLink.setAttribute("href", menuLinks[x].href);
-    aLink.innerHTML = menuLinks[x].text;
-    navMenu.appendChild(aLink);
-    navMenu.firstElementChild.style.color = "red";
+  // creating new element a tag into the nav menu
+  const aLink = document.createElement("a");
+  aLink.setAttribute("href", menuLinks[x].href);
+  aLink.innerHTML = menuLinks[x].text;
+  navMenu.appendChild(aLink);
+  navMenu.firstElementChild.style.color = "red";
+}
 
-  }
-
-  // console.log(aLink)
+// console.log(aLink)
 // =============== here is the form validation part
 
 // validating the form and clearing the input fields
@@ -40,6 +38,23 @@ formReset.addEventListener("submit", (e) => {
   e.preventDefault();
   formReset.reset();
   output.innerHTML += "Form Submitted Successfully!";
-output.innerHTML += '<iframe src="https://giphy.com/embed/H2suj8GiLsEdk9W525" width="480" height="271" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/abcnetwork-shark-tank-H2suj8GiLsEdk9W525">via GIPHY</a></p>';
 
+  output.innerHTML +=
+    '<iframe src="https://giphy.com/embed/H2suj8GiLsEdk9W525" width="480" height="271" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/abcnetwork-shark-tank-H2suj8GiLsEdk9W525">via GIPHY</a></p>';
 });
+
+const footerUl = document.getElementById("ul-footer");
+const gardens = ["The New York Botanical Garden", "Brooklyn Botanical Garden", "Chicago Botanical Garden", "Florida Botanical Garden"];
+
+const fragment = new DocumentFragment();
+
+for (const x of gardens) {
+    const li = document.createElement("li");
+    li.textContent = x;
+    fragment.append(li);
+  }
+  
+  footerUl.append(fragment);
+  footerUl.style.color = "white";
+//   footerUl.classList.add("flexbox");
+
